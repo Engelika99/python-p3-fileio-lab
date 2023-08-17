c9b46f130209f51c4a1ec686fc769336da93ebaf
Alexis Beauregard <mariposa_lex99@yahoo.com>
2023-08-17 19:13:13 -0400

    practice input output with python

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,16 @@
 def write_file(file_name, file_content):
-    pass
+    file_name_str = str(file_name) + '.txt'
+    with open(file_name_str, 'w') as text_file:
+        text_file.write(file_content)
 
 def append_file(file_name, append_content):
-    pass
+    file_name_str = str(file_name) + '.txt'
+    with open(file_name_str, 'a') as text_file:
+        text_file.write(append_content)
+
 
 def read_file(file_name):
-    pass
+    file_name_str = str(file_name) + '.txt'
+    with open(file_name_str, 'r') as text_file:
+        content = text_file.read()
+    return content
